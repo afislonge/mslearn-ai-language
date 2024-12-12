495ee245220c0c2b5a0e640dceef67056f1f586f
afis longe <afislonge@yahoo.com>
2024-12-12 02:45:04 -0500

    update classify code

--- Labfiles/04-text-classification/Python/classify-text/classify-text.py
+++ Labfiles/04-text-classification/Python/classify-text/classify-text.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 import os
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 # Import namespaces
 
@@ -14,7 +17,8 @@ def main():
         deployment_name = os.getenv('DEPLOYMENT')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
         # Read each text file in the articles folder
         batchedDocuments = []
@@ -26,8 +30,22 @@ def main():
             batchedDocuments.append(text)
 
         # Get Classifications
+        operation = ai_client.begin_single_label_classify(
+            batchedDocuments,
+            project_name=project_name,
+            deployment_name=deployment_name
+        )
 
+        document_results = operation.result()
 
+        for doc, classification_result in zip(files, document_results):
+            if classification_result.kind == "CustomDocumentClassification":
+                classification = classification_result.classifications[0]
+                print("{} was classified as '{}' with confidence score {}.".format(
+                doc, classification.category, classification.confidence_score))
+            elif classification_result.is_error is True:
+                print("{} has an error with code '{}' and message '{}'".format(
+                doc, classification_result.error.code, classification_result.error.message))
 
     except Exception as ex:
         print(ex)
